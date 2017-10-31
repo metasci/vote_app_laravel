3c5e97959a1a649fea4bc27a185aa6c465178247
Nick <believewithyoureyes@gmail.com>
2017-10-30 23:13:51 -0400

    db

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('JAWSDB_MARIA_URL', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,7 +46,6 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
